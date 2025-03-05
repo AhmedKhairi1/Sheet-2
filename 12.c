fce13c27ff9603cfdad54728bd930d234ebcca4f
Ahmed Khairi <ahmedkhairi1122@gmail.com>
2025-03-05 20:37:18 +0200

    12.c

--- 12.c
+++ 12.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
-int main()
-{
-    int n, issymmetric=1; scanf("%d",&n);
+
+int main() {
+    int n, issymmetric = 1;
+    scanf("%d", &n);
+    
     int arr[n][n];
+    
     for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
-        {
             scanf("%d", &arr[i][j]);
-        }
-    }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++) 
-        {
-            if (arr[i][j]!=arr[j][i])
-            {
-                issymmetric=0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i][j] != arr[j][i]) {
+                issymmetric = 0;
                 break;
             }
-            if (!issymmetric) break;
         }
-        
-    }    
-    if (issymmetric) {
-        printf("this matrix is symmetric");
-    } else {
-        printf("This matrix is not symmetric");
+        if (!issymmetric) break;
     }
 
+    if (issymmetric) {
+        printf("This matrix is symmetric\n");
+    } else {
+        printf("This matrix is not symmetric\n");
+    }
+
+    return 0;
 }
